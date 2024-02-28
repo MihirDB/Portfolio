b2765a52a440e0a80236254f48a06fc5566ca2df
MihirDB <mihirdb3@gmail.com>
2024-02-28 11:52:59 +0000

    Contact and Navigation bar added

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Active Scrolling
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+// Timeline Animation
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -11,3 +32,16 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Email Sending
+
+function sendMail() {
+    let params = {
+        from_name : document.getElementById("fullName").value,
+        email_id : document.getElementById("email_id").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_wwz5htp", "template_bydbhbo", params).then(function(res) {
+        alert("Succes! " + res.status);
+    })
+};
