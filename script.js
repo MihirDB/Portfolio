254e811d2d525972c60e705c2ec752a259d2f7e8
MihirDB <mihirdb3@gmail.com>
2024-03-01 15:32:02 +0000

    Code formatted

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 // Active Scrolling
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 window.onscroll = () => {
-    sections.forEach(sec => {
+    sections.forEach((sec) => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        let id = sec.getAttribute("id");
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((links) => {
+                links.classList.remove("active");
+                document
+                    .querySelector("header nav a[href*=" + id + "]")
+                    .classList.add("active");
             });
-        };
+        }
     });
 };
 
@@ -21,27 +23,29 @@ window.onscroll = () => {
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        console.log(entry);
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add("show");
         } else {
-            entry.target.classList.remove('show');
-        };
+            entry.target.classList.remove("show");
+        }
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
 // Email Sending
 
 function sendMail() {
     let params = {
-        from_name : document.getElementById("fullName").value,
-        email_id : document.getElementById("email_id").value,
-        message : document.getElementById("message").value
-    }
-    emailjs.send("service_wwz5htp", "template_bydbhbo", params).then(function(res) {
-        alert("Succes! " + res.status);
-    })
-};
+        from_name: document.getElementById("fullName").value,
+        email_id: document.getElementById("email_id").value,
+        message: document.getElementById("message").value,
+    };
+    emailjs
+        .send("service_wwz5htp", "template_bydbhbo", params)
+        .then(function (res) {
+            alert("Succes! " + res.status);
+        });
+}
